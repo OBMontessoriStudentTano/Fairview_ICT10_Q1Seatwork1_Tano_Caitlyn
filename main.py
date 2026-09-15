9769f59d67ec1b25fcae9dd348760b27c0d5b6f7
Lynnie <tano.cr@obmontessori.edu.ph>
2026-09-15 09:01:56 +0800

    Change

---
+++ main.py
@@ -0,0 +1,34 @@
+from pyscript import display, document
+# importing document and display package
+
+My_name = "Caitlyn Anne R. Tano"
+My_age = 15
+My_height = 165.1
+Three_countries_I_visited = ['Singapore', 'Japan', 'Hong Kong']
+Student_Type = False
+Favorite_things = {'Color: Pink', 'Car_brand: Honda', 'Shoe_size: 8 feet' , 'Best_FriendS: The people from my friend group'}
+My_favorite_fruits = set[('Banana','Mango','Strawberry','Apple', 'Grape')]
+Seven_days_of_the_week = ('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday')
+
+
+display(My_name, target="name")
+display(My_age, target="age")
+display(My_height, target="height")
+display(Three_countries_I_visited, target="countries")
+display(Student_Type, target="student")
+display(Favorite_things, target="favorite")
+display(Seven_days_of_the_week, target="weeks")
+
+# def solving:
+   # document.getElementById("Result").innerHTML = "" # clear previous output
+
+    # Number1 = float(document.getElementById("text1").innerHTML) # getting value from an input field
+
+    # Number2 = float(document.getElementById("text2").innerHTML) # getting value from an input field
+
+    # Solve = Number1 + Number2
+
+#     display(result, target="Answer") # displaying the result in the webpage
+
+
+    
