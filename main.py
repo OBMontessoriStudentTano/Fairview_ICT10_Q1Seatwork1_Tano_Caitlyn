cf88ff9796cc7e6cd7e1bcdf0ca71ae53343a2b1
Lynnie <tano.cr@obmontessori.edu.ph>
2026-09-15 10:36:16 +0800

    Change

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ My_age = 15
 My_height = 165.1
 Three_countries_I_visited = ['Singapore', 'Japan', 'Hong Kong']
 Student_Type = False
-Favorite_things = {'Color: Pink', 'Car_brand: Honda', 'Shoe_size: 8 feet' , 'Best_FriendS: The people from my friend group'}
+Favorite_things = {'Color: Pink', 'Car_brand: Honda', 'Shoe_size: 8 feet' , 'Best Friends: My friends from my friend group'}
 My_favorite_fruits = set[('Banana','Mango','Strawberry','Apple', 'Grape')]
 Seven_days_of_the_week = ('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday')
 
@@ -19,16 +19,29 @@ display(Student_Type, target="student")
 display(Favorite_things, target="favorite")
 display(Seven_days_of_the_week, target="weeks")
 
-# def solving:
-   # document.getElementById("Result").innerHTML = "" # clear previous output
+ def solving():
+    document.getElementById("Answer").innerHTML = "" # clear previous output
 
-    # Number1 = float(document.getElementById("text1").innerHTML) # getting value from an input field
+    try:
+     Number1 = float(document.getElementById("text1").value)
+     Number2 = float(document.getElementById("text2").value)
+     Operating = document.getElementById("Operating").value
+    
+    if Operating == "Addition":
+       result = Number1 + Number2
+       symbol = "+"
+    elif Operating == "Subtraction":
+       result = Number1 + Number2
+       symbol = "-"
+    elif Operating == "Multiplication":
+       result = Number1 * Number2
+       symbol = "*"
+    elif Operating == "Division":
+       result = Number1 / Number2
+       symbol = "/"
 
-    # Number2 = float(document.getElementById("text2").innerHTML) # getting value from an input field
-
-    # Solve = Number1 + Number2
-
-#     display(result, target="Answer") # displaying the result in the webpage
+    except ValueError:
+        display("Please enter valid numbers in both boxes", target="Answer")
 
 
     
